f5e1ef27f955f7c263ff619732f9b907e9e462d3
Simeon Georgiev <sgeorgiev783@gmail.com>
2022-10-27 22:41:27 +0300

    added sidebar and navbar

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import React from 'react'
+
+const Home: NextPage = () => {
+  return (
+    <h1 className='text-3x1 font-bold underline'>
+      TikTIk
+    </h1>
+  )
+}
+
+export default Home
