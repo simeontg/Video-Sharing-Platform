0d1c65fe8efac1209831c996d9571f2fafb39ba0
Simeon Georgiev <sgeorgiev783@gmail.com>
2022-11-02 13:44:26 +0200

    added upload video functionality

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -68,12 +68,12 @@ const VideoCard: NextPage<IProps> = ({post}) => {
 
         <div className='lg:ml-20 flex gap-4 relative'>
             <div className='rounded-3xl' onMouseEnter={() => {setIsHover(true)}} onMouseLeave={() => {setIsHover(false)}}>
-                <Link href='/'>
+                <Link href={`/detail/${post._id}`}>
                     <video 
                     ref={videoRef}
                     src={post.video.asset.url}
                     loop
-                    className='lg:w[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100'
+                    className='lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100'
                     >
                     </video>
                 </Link>
