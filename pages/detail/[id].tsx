e923ff26c72bd2d0895ddc44dfc1ea3e337feba1
Simeon Georgiev <sgeorgiev783@gmail.com>
2022-11-03 23:37:40 +0200

    added detail page

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,11 +1,132 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+import Link from 'next/link'
+import { GoVerified } from 'react-icons/go'
+import { MdOutlineCancel } from 'react-icons/md'
+import { BsFillPlayFill } from 'react-icons/bs'
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi'
+import axios from 'axios'
+import { BASE_URL } from '../../utils'
+import useAuthStore from '../../store/authStore'
+import Comments from '../../components/Comments'
+import LikeButton from '../../components/LikeButton'
 
-const Detail = () => {
+
+const Detail = ({postDetails}) => {
+    const [post, setPost] = useState(postDetails)
+    const [playing, setPlaying] = useState(false)
+    const [isMuted, setIsMuted] = useState(false)
+    const videoRef = useRef(null)
+    const router = useRouter()
+    const {userProfile} = useAuthStore()
+
+    const onVideoClick = () => {
+        if(playing){
+            setPlaying(false)
+            videoRef.current.pause()
+        }else{
+            setPlaying(true)
+            videoRef.current.play()
+        }
+    }
+
+    const onMute = () => {
+        if(isMuted){
+            videoRef.current.muted = false
+            setIsMuted(false)
+        }else{
+            videoRef.current.muted = true
+            setIsMuted(true)
+        }
+    }
+
+    if(!post) return null
+
   return (
-    <div>
-        
+    <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
+        <div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-black'>
+            <div className='absolute top-6 left-2 lg:left-6 flex gap-6 z-50'>
+                <p className='cursor-pointer' onClick={() => router.back()}>
+                    <MdOutlineCancel className='text-white text-[35px]'/>
+                </p>
+            </div>
+            <div className='relative'>
+                <div className='lg:h-[100vh] h-[60vh]'>
+                    <video
+                    src={post.video.asset.url}
+                    className='h-full cursor-pointer'
+                    ref={videoRef}
+                    loop
+                    onClick={onVideoClick}
+                    >
+                    </video>
+                </div>
+                <div className='absolute top-[45%] left-[45%] cursor-pointer'>
+                    {!playing && (
+                        <button onClick={onVideoClick}>
+                            <BsFillPlayFill className='text-white text-6xl lg:text-8xl'/>
+                        </button>
+                    )}
+                </div>
+            </div>
+            <div className='absolute bottom-5 lg:bottom-10 right-5 lg:right-10 cursor-pointer'>
+            {isMuted ? (
+                            <button onClick={onMute}>
+                                <HiVolumeOff className='text-white text-2xl lg:'/>
+                            </button>
+                        ) : <button onClick={onMute}>
+                                 <HiVolumeUp className='text-white text-2xl lg:text-4xl'/>
+                            </button>
+                        }
+            </div>
+        </div>
+        <div className='relative w-[1000px] md:w-[900px] lg:w-[700px]'>
+            <div className='lg:mt-20 mt-10'>
+              <div className='flex gap-3 padding-2 cursor-pointer font-semibold rounded'>
+                <div className='ml-4 md:w-20 md:h-20 w-16 h-16'>
+                    <Link href='/'>
+                        <>
+                        <Image width={62} height={62} className='rounded-full' src={post.postedBy?.image} alt='profile-picture' layout='responsive'/>
+                        </>
+                    </Link>
+                </div>
+                <div>
+                    <Link href='/'>
+                        <div className='flex flex-col gap-2 mt-3'>
+                            <p className='flex gap-2 items-center md:text-md font-bold text-primary'>
+                                {post.postedBy.userName} {``}
+                                <GoVerified className='text-blue-400 text-md'/>
+                            </p>
+                            <p className='capitalize font-medium text-xs text-gray-500 hidden md:block'>
+                                {post.postedBy.userName}
+                            </p>
+                        </div>
+                    </Link>
+                </div>
+              </div>
+              <p className='p-5 text-md text-gray-600 text-lg'>{post.caption}</p>
+              <div className='mt-10 px-10'>
+                {userProfile && (
+                    <LikeButton />
+                )}
+              </div>
+              <Comments />
+
+            </div>
+        </div>
     </div>
   )
+}
+
+export const getServerSideProps = async({params: { id }}) => {
+  console.log(`${BASE_URL}}/api/post/${id}`)
+
+  const {data} = await axios.get(`${BASE_URL}/api/post/${id}`)
+
+  return {
+    props: { postDetails: data }
+  }
 }
 
 export default Detail
--- utils/queries.tsx
+++ utils/queries.tsx
@@ -28,3 +28,32 @@ export const allPostsQuery = () => {
   
     return query;
   };
+
+  export const postDetailQuery = (postId: string | string[]) => {
+    const query = `*[_type == "post" && _id == '${postId}']{
+      _id,
+       caption,
+         video{
+          asset->{
+            _id,
+            url
+          }
+        },
+        userId,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+       likes,
+      comments[]{
+        comment,
+        _key,
+        postedBy->{
+          _ref,
+        _id,
+      },
+      }
+    }`;
+    return query;
+  };
